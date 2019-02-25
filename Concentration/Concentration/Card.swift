8ec20290081ed21bc327d7adbce41a4612d0173d
Stuart Fooks <sb.fooks@gmail.com>
2019-02-25 16:06:09 -0500

    set up card model

---
+++ Concentration/Concentration/Card.swift
@@ -0,0 +1,19 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Stuart Fooks on 2/25/19.
+//  Copyright © 2019 Stuart Fooks. All rights reserved.
+//
+
+import Foundation
+
+//struct is different from class with 2 main differences:
+//1- no inheritence
+//2- structs are value types: they get copied when they are used in other areas
+//classes are reference types: you point to it
+struct Card {
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+}
---
+++ Concentration/Concentration/Concentration.swift
@@ -0,0 +1,17 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Stuart Fooks on 2/25/19.
+//  Copyright © 2019 Stuart Fooks. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    var cards: Array<Card>
+    
+    func ChooseCard(at index: Int) {
+        
+    }
+}
